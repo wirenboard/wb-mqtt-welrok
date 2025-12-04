224d6ba5a5ad544bf0c2fdc01df9d161b9681014
victor.fedorov <victor.fedorov@wirenboard.com>
2025-12-04 17:21:31 +0300

    fix: test version init

--- wb_welrok/__init__.py
+++ wb_welrok/__init__.py
@@ -1,7 +1,1 @@
-from importlib.metadata import PackageNotFoundError, version
-
 __version__ = "0.0.16"
-try:
-    __version__ = version("wb-mqtt-welrok")
-except PackageNotFoundError:
-    __version__ = "unknown"
